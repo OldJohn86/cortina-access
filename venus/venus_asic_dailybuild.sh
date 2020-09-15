29f995410c1382e99a520680574262c0d241b6d5
Pengpeng Chen <pengpeng.chen@cortina-access.com>
2020-09-15 16:54:03 +0800

    Add build procedures for target g3/g3hgu/venus

---
+++ venus/venus_asic_dailybuild.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Latest date: 2020-09-15
+
+BUILD_DIR=venus_yocto
+LOG_FILE="venus_dailybuild.txt"
+TAIL_FILE="./tail.txt"
+DayTimeTag=$(date "+%Y%m%d")
+MonTimeTag=$(date "+%Y%m")
+YestTag=$(date -d '3 days ago' +%Y%m%d)
+WORK_DIR=/space/pchen/cortina-access/venus/
+
+cd $WORK_DIR
+git clone -b ca-zeus-master gitolite@cs-local-gitmirror:sw/platforms/cs-unified/distro/yocto $BUILD_DIR-$DayTimeTag
+cd $BUILD_DIR-$DayTimeTag
+git clone -b ca-zeus-master gitolite@cs-local-gitmirror:sw/platforms/cs-unified/distro/openembedded/meta-openembedded
+# git clone -b ca-zeus-master gitolite@cs-local-gitmirror:sw/platforms/cs-unified/distro/openembedded/meta-virtualization
+git clone -b ca-zeus-master gitolite@cs-local-gitmirror:sw/platforms/cs-unified/distro/meta-oe-yocto-cortina
+git clone -b ca-zeus-master gitolite@cs-local-gitmirror:sw/platforms/cs-unified/distro/meta-ca-bsp
+export TEMPLATECONF=meta-ca-bsp/conf/venus-eng/
+source ./oe-init-build-env
+
+# Make build
+bitbake major-image &> $WORK_DIR/$LOG_FILE &
